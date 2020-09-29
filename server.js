3961f33d340f5efb9a82fe3af495ea075c59819f
Nicole Gile <nicolegile94@gmail.com>
2020-09-29 00:14:29 -0500

    refactor

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const apiRoutes = require('./routes');
+const htmlRoutes = require('./routes');
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
@@ -5,30 +7,11 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+app.use(express.static('public'));
 const { notes } = require('./db/db');
 
-function createNewNote(body, notesArray) {
-    const note = body;
-    notesArray.push(note);
-    fs.writeFileSync(
-        path.join(__dirname, './db/db.json'),
-        JSON.stringify({ notes: notesArray }, null, 2)
-    );
-    return note;
-}
-
-app.get('/api/notes', (req, res) => {
-    let results = notes;
-    res.json(results);
-});
-
-app.post('/api/notes', (req, res) => {
-    req.body.id = notes.length.toString();
-
-    const note = createNewNote(req.body, notes);
-
-    res.json(note);
-});
 
 app.listen(PORT, () => {
     console.log(`API server is not on port ${PORT}`);
