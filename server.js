be8753b0881e010c4764d1911d68c6f3bfad07ac
Nicole Gile <nicolegile94@gmail.com>
2020-09-28 23:29:00 -0500

    add post

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 const { notes } = require('./db/db');
+
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
+    return note;
+}
 
 app.get('/api/notes', (req, res) => {
     let results = notes;
     res.json(results);
+});
+
+app.post('/api/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+
+    const note = createNewNote(req.body, notes);
+
+    res.json(note);
 });
 
 app.listen(PORT, () => {
