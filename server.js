52502181ac60ba0a440baeaac10161eda9009401
Nicole Gile <nicolegile94@gmail.com>
2020-09-30 12:36:41 -0500

    fix the things I broke

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
@@ -7,11 +5,39 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
 app.use(express.static('public'));
 const { notes } = require('./db/db');
 
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
+    return note;
+}
+
+app.get('/api/notes', (req, res) => {
+    let results = notes;
+    res.json(results);
+});
+
+app.post('/api/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+
+    const note = createNewNote(req.body, notes);
+
+    res.json(note);
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`API server is not on port ${PORT}`);
